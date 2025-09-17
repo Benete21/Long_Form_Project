557880e424d82eb133d1c45120aa30aa9cfa8b53
benete21 <benyubun@gmail.com>
2025-09-17 16:20:58 +0200

    adding colliders for tutorial

---
+++ Long_Form_Project/Assets/Scripts/OpenUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class OpenUI : MonoBehaviour
+{
+    [Header("UI to Open")]
+    public GameObject uiPanel;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (uiPanel == null)
+            {
+                uiPanel.SetActive(true);
+                Debug.Log("CONTACT!");
+            }
+        }
+    }
+}
