5dbd5da027f4fe9470987c380731003cb7d5497e
benete21 <benyubun@gmail.com>
2025-09-26 22:09:05 +0200

    trying to add laser indidcator
    still having issues with making it work

--- Long_Form_Project/Assets/Scripts/Laser_Reflect_Mirror.cs
+++ Long_Form_Project/Assets/Scripts/Laser_Reflect_Mirror.cs
@@ -7,44 +7,91 @@ public class Laser_Reflect_Mirror : MonoBehaviour
     Vector3 pos, dir;
 
     GameObject laserObj;
-    LineRenderer laser;
-    List<Vector3> laserIndices = new List<Vector3>();
+    public LineRenderer laser;
+    public List<Vector3> laserIndices = new List<Vector3>();
+    public int length;
+    public OpenDoor od;
 
-    public Laser_Reflect_Mirror(Vector3 pos, Vector3 dir, Material mat)
+    public void Start()
     {
-        this.laser = new LineRenderer();
-        this.laserObj = new GameObject();
-        this.pos = pos;
-        this.dir = dir;
+        laser = GetComponent<LineRenderer>();
+        od = FindObjectOfType<OpenDoor>();
+    }
+    public void InitializeLaser(Vector3 startPos, Vector3 direction, Material mat)
+    {
+        pos = startPos;
+        dir = direction;
 
-        this.laser = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
-        this.laser.startWidth = 0.1f;
-        this.laser.endWidth = 0.1f;
-        this.laser.material = mat;
-        this.laser.startColor = Color.green;
-        this.laser.endColor = Color.green;
+        if (laser == null)
+        {
+            laserObj = new GameObject("LaserLine");
+            laser = laserObj.AddComponent<LineRenderer>();
+
+            laser.startWidth = 0.1f;
+            laser.endWidth = 0.1f;
+            laser.material = mat;
+            laser.startColor = Color.green;
+            laser.endColor = Color.green;
+        }
 
         CastRay(pos, dir, laser);
-        
     }
 
-    void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser)
+
+    public void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser, int depth = 0)
     {
-        laserIndices.Add(pos);
+        if (depth == 0)
+            laserIndices.Clear();
+
+        if (depth > 10) // Avoid infinite recursion
+        {
+            laserIndices.Add(pos + dir * 10f);
+            UpdateLaser();
+            return;
+        }
+
+        laserIndices.Add(pos); // Add the current ray start position
 
         Ray ray = new Ray(pos, dir);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, 30, 1))
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            CheckHit(hit, dir, laser);
+            laserIndices.Add(hit.point); // Add hit point
+
+            if (hit.collider.CompareTag("Mirror"))
+            {
+                Vector3 reflectDir = Vector3.Reflect(dir, hit.normal);
+                Vector3 reflectOrigin = hit.point + reflectDir * 0.01f; // small offset
+
+                CastRay(reflectOrigin, reflectDir, laser, depth + 1);
+            }
+            else if (hit.collider.CompareTag("LaserButton"))
+            {
+                var door = hit.collider.GetComponent<OpenDoor>();
+                if (door != null)
+                {
+                    door.OpenDoorAnimator();
+                }
+
+                UpdateLaser();
+            }
+            else
+            {
+                // Hit something else
+                UpdateLaser();
+            }
         }
         else
         {
-            laserIndices.Add(ray.GetPoint(30));
+            // Hit nothing
+            laserIndices.Add(pos + dir * 70f);
             UpdateLaser();
         }
     }
+
+
+
 
     void UpdateLaser()
     {
@@ -55,26 +102,6 @@ public class Laser_Reflect_Mirror : MonoBehaviour
         {
             laser.SetPosition(count, idx);
             count++;
-        }
-    }
-
-    void CheckHit(RaycastHit hitinfo, Vector3 direction, LineRenderer laser)
-    {
-        if (hitinfo.collider.gameObject.CompareTag("Mirror"))
-        {
-            Vector3 pos = hitinfo.point;
-            Vector3 dir = Vector3.Reflect(direction, hitinfo.normal);
-
-            CastRay(pos, dir, laser);
-        }
-        else if (hitinfo.collider.gameObject.CompareTag("LaserButton"))
-        {
-            print("ajfgdfwqwdgmfndbsgehrjdtgmnfbdgehrdjfgmcnvbdgdf");
-        }
-        else
-        {
-            laserIndices.Add(hitinfo.point);
-            UpdateLaser();
         }
     }
 }
--- Long_Form_Project/Assets/Scripts/Shoot_Laser_Mirror.cs
+++ Long_Form_Project/Assets/Scripts/Shoot_Laser_Mirror.cs
@@ -1,15 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot_Laser_Mirror : MonoBehaviour
 {
     public Material material;
-    public Laser_Reflect_Mirror beam;
+    private Laser_Reflect_Mirror beam;
 
-    void Update()
+    private void Start()
     {
-        Destroy(GameObject.Find("New Game Object"));
-       beam = new Laser_Reflect_Mirror(gameObject.transform.position, gameObject.transform.right, material); 
+        // Get the Laser_Reflect_Mirror component attached to the same GameObject
+        beam = GetComponent<Laser_Reflect_Mirror>();
+        if (beam == null)
+        {
+            beam = gameObject.AddComponent<Laser_Reflect_Mirror>();
+        }
+        beam.InitializeLaser(transform.position, transform.right, material);
+    }
+
+    private void Update()
+    {
+        if (beam == null || beam.laser == null)
+            return;
+
+        beam.laser.positionCount = 0;
+        beam.laserIndices.Clear();
+        beam.CastRay(transform.position, transform.right, beam.laser);
     }
 }
